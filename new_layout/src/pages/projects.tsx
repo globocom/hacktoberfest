cbdeb70f9e7ee0315f5bbc9faf0ac702b43640e3
Lourenço Sander <lourenco.sander@g.globo>
2021-08-27 17:48:32 -0300

    Start project pages development

--- new_layout/src/pages/projects.tsx
+++ new_layout/src/pages/projects.tsx
@@ -1,4 +1,6 @@
 import React from "react"
+import { Typography } from "@material-ui/core"
+import Spacing from "@components/spacing"
 import Layout from "@components/layout"
 import SEO from "@components/seo"
 
@@ -6,7 +8,16 @@ const ProjectsPage = () => {
   return (
     <Layout>
       <SEO description="Globo Hacktoberfest" title="Projetos" />
-      <h1> Index Page</h1>
+      <Spacing smart={{ margin: "64px 0px 24px" }}>
+        <Typography
+          component="p"
+          align="center"
+          color="primary"
+          variant="body1"
+        >
+          Gostaríamos da sua ajuda principalmente nos seguintes projetos:
+        </Typography>
+      </Spacing>
     </Layout>
   )
 }
