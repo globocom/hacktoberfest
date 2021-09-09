091e2fe395f582eb8ddfc77a503015c3a545ab3c
Laura Ceconi <laura.ceconi@g.globo>
2021-09-09 17:40:51 -0300

    feat: Change page title

--- new_layout/src/pages/participants.tsx
+++ new_layout/src/pages/participants.tsx
@@ -1,12 +1,38 @@
 import React from "react"
 import Layout from "@components/layout"
 import SEO from "@components/seo"
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@material-ui/core"
+import Spacing from "@components/spacing"
 
 const ParticipantsPage = () => {
   return (
-    <Layout  title="Minha Área - Globo Hacktoberfest" description="Minha Área - Globo Hacktoberfest" headerTitle="Minha área">
+    <Layout title="Participantes - Globo Hacktoberfest" description="Participantes - Globo Hacktoberfest" headerTitle="Participantes">
       <SEO description="Globo Hacktoberfest" title="Participantes" />
-      <h1> Participants Page</h1>
+      <Spacing smart={{padding: "0px 40px"}}>
+
+      <List>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Brunch this weekend?"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                color="textPrimary"
+                >
+                Ali Connors
+              </Typography>
+              {" — I'll be in your neighborhood doing errands this…"}
+            </React.Fragment>
+          }
+          />
+      </ListItem>
+      </List>
+          </Spacing>
     </Layout>
   )
 }
