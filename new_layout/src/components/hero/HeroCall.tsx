a98d2612db8db659d790ac06707a9e391cd62bbd
Lourenço Sander <lourenco.sander@g.globo>
2021-09-17 17:54:21 -0300

    WWIP: Desktop index page

--- new_layout/src/components/hero/HeroCall.tsx
+++ new_layout/src/components/hero/HeroCall.tsx
@@ -1,117 +1,203 @@
-
 import React from "react"
-import Spacing from '@components/spacing'
-import {makeStyles, Theme} from '@material-ui/core/styles'
+import Spacing from "@components/spacing"
+import { makeStyles, Theme } from "@material-ui/core/styles"
 import { Grid, Hidden, Typography, Button } from "@material-ui/core"
-import { Image } from '@components/image'
-import ArrowDownIcon from '@material-ui/icons/ArrowDownward'
-
-
+import { Image } from "@components/image"
+import ArrowDownIcon from "@material-ui/icons/ArrowDownward"
 
 interface ImageHero {
-  image: string,
-  format: string,
-  repeat?: number,
+  image: string
+  format: string
+  repeat?: number
   initialPosition: {
-    x: number,
+    x: number
     y: number
-  },
+  }
 }
 
 interface DraggableContentProps {
-  image: ImageHero,
+  image: ImageHero
 
   children?: React.ReactNode
 }
 
-
 const useStyles = makeStyles((theme: Theme) => ({
-    heroPanel: {
-      position: 'relative',
-      overflow: 'hidden',
-      backgroundColor: theme.palette.primary.main,
-      height: "93.3vh",
-      width: "100%",
-      zIndex: 1,
-      [theme.breakpoints.up("md")]: {
-        height: "92vh",
-      },
+  heroPanel: {
+    position: "relative",
+    overflow: "hidden",
+    backgroundColor: theme.palette.primary.main,
+    height: "93.3vh",
+    width: "100%",
+    zIndex: 1,
+    [theme.breakpoints.up("md")]: {
+      height: "92vh",
     },
+  },
 }))
 
-
 const SmartView = () => {
-
   return (
     <React.Fragment>
-      <Spacing desktop={{margin: "0px 0px 72px"}} smart={{margin: "0px 0px 24px"}}>
-        <Grid container alignItems="center" alignContent="center" justifyContent="center">
-            <Spacing smart={{margin: "20px 0px 0px"}}>
-              <Grid item xs={12}>
-                  <Typography variant="h2" align="center" component="h2"> 01 a 31 de outubro</Typography>
-              </Grid>
-            </Spacing>
-            <Spacing smart={{margin: "40px 0px 0px"}}>
-              <Grid item xs={12}>
-                  <Image style={{width: "90%", display: "block", margin: "0px auto"}} src={`hero/hero.svg`} />
-              </Grid>
-            </Spacing>
-            <Spacing smart={{margin: "60px 0px 0px"}}>
-              <Grid item xs={8}>
-                <Typography align="center" variant="h3" component="h3">Contribua e ganhe uma camiseta exclusiva</Typography>
-              </Grid>
-            </Spacing>
-            <Spacing smart={{margin: "40px 0px 0px"}}>
-              <Grid item xs={8}>
-                <Button style={{"backgroundColor": "#fff", borderRadius: "64px"}} fullWidth variant="contained">
-                  <pre style={{fontFamily: "inherit"}}>
-                    <b>Participar</b>
-                    <p>com sua conta do github</p>
-                  </pre>
-                </Button>
-              </Grid>
-            </Spacing>
-            <Grid item>
-                <Image src={`hero/beer.svg`} style={{"position": "absolute", left: "-30px", top: "35%", zIndex: "-1"}}/>
-                <Image src={`hero/major_tom.svg`}  style={{"position": "absolute", right:"-33px", width: "33%", top: "67%", zIndex: "-1"}}/>
+      <Spacing
+        desktop={{ margin: "0px 0px 72px" }}
+        smart={{ margin: "0px 0px 24px" }}
+      >
+        <Grid
+          container
+          alignItems="center"
+          alignContent="center"
+          justifyContent="center"
+        >
+          <Spacing smart={{ margin: "20px 0px 0px" }}>
+            <Grid item xs={12}>
+              <Typography variant="h2" align="center" component="h2">
+                {" "}
+                01 a 31 de outubro
+              </Typography>
             </Grid>
+          </Spacing>
+          <Spacing smart={{ margin: "40px 0px 0px" }}>
+            <Grid item xs={12}>
+              <Image
+                style={{ width: "90%", display: "block", margin: "0px auto" }}
+                src={`hero/hero.svg`}
+              />
+            </Grid>
+          </Spacing>
+          <Spacing smart={{ margin: "60px 0px 0px" }}>
+            <Grid item xs={8}>
+              <Typography align="center" variant="h3" component="h3">
+                Contribua e ganhe uma camiseta exclusiva
+              </Typography>
+            </Grid>
+          </Spacing>
+          <Spacing smart={{ margin: "40px 0px 0px" }}>
+            <Grid item xs={8}>
+              <Button
+                style={{ backgroundColor: "#fff", borderRadius: "64px" }}
+                fullWidth
+                variant="contained"
+              >
+                <pre style={{ fontFamily: "inherit" }}>
+                  <b>Participar</b>
+                  <p>com sua conta do github</p>
+                </pre>
+              </Button>
+            </Grid>
+          </Spacing>
+          <Grid item>
+            <Image
+              src={`hero/beer.svg`}
+              style={{
+                position: "absolute",
+                left: "-30px",
+                top: "35%",
+                zIndex: "-1",
+              }}
+            />
+            <Image
+              src={`hero/major_tom.svg`}
+              style={{
+                position: "absolute",
+                right: "-33px",
+                width: "33%",
+                top: "67%",
+                zIndex: "-1",
+              }}
+            />
+          </Grid>
         </Grid>
       </Spacing>
 
-      <Spacing smart={{margin: "32px 0px 0px"}}>
-          <Grid container justifyContent="center" alignItems="center">
-            <Grid item>
-                <ArrowDownIcon/>
-            </Grid>
-            <Grid item>
-                <Typography align="center" component="p"> como funciona </Typography>
-            </Grid>
+      <Spacing smart={{ margin: "32px 0px 0px" }}>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item>
+            <ArrowDownIcon />
           </Grid>
+          <Grid item>
+            <Typography align="center" component="p">
+              {" "}
+              como funciona{" "}
+            </Typography>
+          </Grid>
+        </Grid>
       </Spacing>
-
     </React.Fragment>
   )
 }
 
 const DesktopView = () => {
-
   return (
-    <h1> Desktop </h1>
+    <React.Fragment>
+      <Spacing
+        desktop={{ margin: "0px 0px 72px" }}
+        smart={{ margin: "0px 0px 24px" }}
+      >
+        <Grid
+          container
+          alignItems="center"
+          alignContent="center"
+          justifyContent="center"
+        >
+          <Spacing smart={{ margin: "16px 0px" }}>
+            <Grid item xs={12}>
+              <Typography variant="h2" align="center" component="h2">
+                {" "}
+                01 a 31 de outubro
+              </Typography>
+            </Grid>
+          </Spacing>
+          <Grid item xs={8}>
+            <Typography align="center" variant="h3" component="h3">
+              Contribua e ganhe uma camiseta exclusiva
+            </Typography>
+          </Grid>
+          <Spacing smart={{ margin: "40px 0px 0px" }}>
+            <Grid item xs={8}>
+              <Button
+                style={{ backgroundColor: "#fff", borderRadius: "64px" }}
+                fullWidth
+                variant="contained"
+              >
+                <pre style={{ fontFamily: "inherit" }}>
+                  <b>Participar</b>
+                  com sua conta do github
+                </pre>
+              </Button>
+            </Grid>
+          </Spacing>
+        </Grid>
+      </Spacing>
+
+      <Spacing smart={{ margin: "32px 0px 0px" }}>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item>
+            <ArrowDownIcon />
+          </Grid>
+          <Grid item>
+            <Typography align="center" component="p">
+              {" "}
+              como funciona{" "}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Spacing>
+    </React.Fragment>
   )
 }
-  
-const HeroCall = () => {
-    const classes = useStyles()
 
-    return (
-        <div className={classes.heroPanel} id="hero_panel">
-          <Hidden smDown>
-            <DesktopView/>
-          </Hidden>
-          <Hidden mdUp>
-            <SmartView/>
-          </Hidden>
-            {/*<Spacing desktop={{margin: "0px 0px 72px"}} smart={{margin: "0px 0px 24px"}}>
+const HeroCall = () => {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.heroPanel} id="hero_panel">
+      <Hidden smDown>
+        <DesktopView />
+      </Hidden>
+      <Hidden mdUp>
+        <SmartView />
+      </Hidden>
+      {/*<Spacing desktop={{margin: "0px 0px 72px"}} smart={{margin: "0px 0px 24px"}}>
               <Grid direction="column" container alignItems="center" alignContent="center">
                   <Spacing desktop={{margin: "0px 0px 24px"}} smart={{margin: "0px 0px 38px"}}>
                     <Grid item>
@@ -124,9 +210,8 @@ const HeroCall = () => {
                   </Spacing>
                 </Grid>
                         </Spacing>*/}
-          </div>
-    )
+    </div>
+  )
 }
-
 
 export default HeroCall
--- new_layout/src/pages/index.tsx
+++ new_layout/src/pages/index.tsx
@@ -1,13 +1,14 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import Layout from "@components/layout"
-import Spacing from '@components/spacing'
-import HeroCall from '@components/hero'
-import {makeStyles, Theme} from '@material-ui/core/styles'
-import { Grid, Typography, Avatar } from "@material-ui/core"
-import "~slick-carousel/slick/slick.css"; 
-import "~slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
-
+import Spacing from "@components/spacing"
+import HeroCall from "@components/hero"
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import { Grid, Typography, IconButton } from "@material-ui/core"
+import NavigateNextIcon from "@material-ui/icons/NavigateNext"
+import NavigateBeforesIcon from "@material-ui/icons/NavigateBefore"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import Slider from "react-slick"
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -30,114 +31,121 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   rounded: {
     borderRadius: 50,
-    fontWeight: "normal"
-  }
+    fontWeight: "normal",
+  },
+  ruleText: {
+    fontSize: 56,
+  },
 }))
 
-const Rule = () => {
+const Rule = ({ rule, index }) => {
   const classes = useStyles()
   return (
-    <Grid container justifyContent="center" alignItems="center">
-      <Grid item>
-        <Spacing
-          desktop={{ margin: "0px 0px 16px" }}
-          smart={{ margin: "0px 0px 20px" }}
-        >
-          <Avatar className={classes.large} variant="rounded"></Avatar>
-        </Spacing>
+    <Spacing smart={{ padding: "0px 200px" }}>
+      <Grid container direction="row" justifyContent="center">
+        <Grid container item direction="column" alignItems="flex-start" md={2}>
+          <Typography variant="h1" color="textPrimary">
+            {index + 1}.
+          </Typography>
+        </Grid>
+        <Grid container item md={8}>
+          <Typography
+            variant="h1"
+            color="textPrimary"
+            className={classes.ruleText}
+          >
+            {rule}
+          </Typography>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Typography variant="body2">
-          {" "}
-          Contribua com dois PRs em qualquer projeto Open Source da Globo
-          durante o mês de outubro.{" "}
-        </Typography>
-      </Grid>
-    </Grid>
+    </Spacing>
   )
 }
 
+const CustomNextArrow = (props) => {
+  const { onClick, style } = props
 
-const IndexRules = () => {
-  const classes = useStyles();
   return (
-    <Grid container className={classes.info}>
-      <Spacing desktop={{ padding: "40px" }} smart={{ padding: "20px" }}>
-        <Grid item xs={12} md={4} lg={4}>
-          <Rule />
-        </Grid>
-      </Spacing>
-      <Spacing desktop={{ padding: "40px" }} smart={{ padding: "20px" }}>
-        <Grid item xs={12} md={4} lg={4}>
-          <Rule />
-        </Grid>
-      </Spacing>
-      <Spacing desktop={{ padding: "40px" }} smart={{ padding: "20px" }}>
-        <Grid item xs={12} md={4} lg={4}>
-          <Rule />
-        </Grid>
-      </Spacing>
-    </Grid>
+    <IconButton onClick={onClick} style={style}>
+      <NavigateNextIcon />
+    </IconButton>
   )
 }
 
-const HacktoberfestCarousel = () => {
+const CustomPreviousArrow = (props) => {
+  const { onClick, style } = props
+
+  return (
+    <IconButton onClick={onClick} style={style}>
+      <NavigateBeforesIcon />
+    </IconButton>
+  )
+}
+
+const HacktoberfestCarousel = (props) => {
+  const classes = useStyles()
+  const { rules } = props
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
-  };
+    slidesToScroll: 1,
+    nextArrow: <CustomNextArrow />,
+    prevArrow: <CustomPreviousArrow />,
+  }
 
   return (
-    <div>
-        <h2> Single Item</h2>
+    <Spacing desktop={{ margin: "40px auto" }} smart={{ margin: "20px auto" }}>
+      <div style={{ width: "80%" }}>
         <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
+          {rules.map((rule, index) => {
+            return <Rule rule={rule} index={index} />
+          })}
         </Slider>
       </div>
+    </Spacing>
   )
-
 }
 
 const IndexPage = () => {
   const classes = useStyles()
+
+  const rules = [
+    "Contribua com dois PRs em qualquer projeto Open Source da Globo durante o mês de outubro.",
+    "Garanta que pelo menos um pull request seja ACEITO.",
+    "Os 100 primeiros inscritos que completarem os requisitos mínimos ganharão uma camiseta.*",
+  ]
+
   return (
-      <Layout title="Início - Globo Hacktoberfest">
-        <div className={classes.root}>
-          <HeroCall/>
-          <HacktoberfestCarousel/>
-          <Spacing smart={{margin: "0px 0px 64px"}}>
-            <Grid container direction="column" justifyContent="center" alignItems="center">
-                <Grid item xs={12} md={12} lg={12}>
-                    <Typography variant="h2" color="secondary">Projetos</Typography>
-                </Grid>
-                <Grid item xs={8} md={6} lg={6}>
-                  <Typography align="center" variant="body1" color="textPrimary">Some of the most popular developer tools, frameworks and experiences in the world are built around open communities. Here are a few featured Microsoft projects of note.</Typography>
-                </Grid>
+    <Layout title="Início - Globo Hacktoberfest">
+      <div className={classes.root}>
+        <HeroCall />
+        <HacktoberfestCarousel rules={rules} />
+        <Spacing smart={{ margin: "0px 0px 64px" }}>
+          <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={12} md={12} lg={12}>
+              <Typography variant="h2" color="secondary">
+                Projetos
+              </Typography>
             </Grid>
-          </Spacing>
-        </div>
-      </Layout>
-    )
+            <Grid item xs={8} md={6} lg={6}>
+              <Typography align="center" variant="body1" color="textPrimary">
+                Some of the most popular developer tools, frameworks and
+                experiences in the world are built around open communities. Here
+                are a few featured Microsoft projects of note.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Spacing>
+      </div>
+    </Layout>
+  )
 }
 
 export default IndexPage
