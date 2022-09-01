39d9ce92c965a203db20eaee31db9dadaf6f811a
GabhenDM <henriques.gabriel@outlook.com>
2022-09-01 17:40:39 -0300

    wip: projects list main page beginning

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,40 +1,37 @@
-
-
-const API_URL: string = process.env.GATSBY_API_URL || ''
+const API_URL: string = process.env.GATSBY_API_URL || "http://localhost:3000"
 
 interface ApiResponse {
-    data: any
+  data: any
 }
 
 interface Request {
-    path: string;
-    method: string;
-    body?: any;
-    sendCookies?: boolean;
+  path: string
+  method: string
+  body?: any
+  sendCookies?: boolean
 }
 
-const doRequest = async (request: Request) : Promise<ApiResponse> => {
-    try{
-        const response = await fetch(`${API_URL}${request.path}`, {
-            method: request.method || "GET",
-            credentials: request.sendCookies ? "include" : "omit",
-            body: request.body && JSON.stringify(request.body),
-        })
+const doRequest = async (request: Request): Promise<ApiResponse> => {
+  try {
+    const response = await fetch(`${API_URL}${request.path}`, {
+      method: request.method || "GET",
+      credentials: request.sendCookies ? "include" : "omit",
+      body: request.body && JSON.stringify(request.body),
+    })
 
-        if(response.status >= 400){
-            throw new Error(`[API ERROR] Fail to ${request.path}.`)
-        }
-
-        const data = await response.json()
-        return Promise.resolve({data: data.result})
-    }catch(error){
-        return Promise.reject({
-            data: {},
-            message: `Failed to request -> ${request.path}.`,
-            cause: error
-        })
+    if (response.status >= 400) {
+      throw new Error(`[API ERROR] Fail to ${request.path}.`)
     }
 
+    const data = await response.json()
+    return Promise.resolve({ data: data.result })
+  } catch (error) {
+    return Promise.reject({
+      data: {},
+      message: `Failed to request -> ${request.path}.`,
+      cause: error,
+    })
+  }
 }
 
 export default doRequest
