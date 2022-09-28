c78d2d35d706493559e66bb123bf2a6584ba729d
Marcus Cartágenes <mvcartagenes@gmail.com>
2022-09-27 22:36:06 -0300

    Centering rules pages

--- src/pages/regras.tsx
+++ src/pages/regras.tsx
@@ -46,59 +46,65 @@ const RuleBookPage = () => {
       title="Regras e Princípios - Globo Hacktoberfest"
       description="Regras e Princípos - Globo Hacktoberfest"
       headerTitle="Regras e Princípios">
-        <Grid container direction="column" justifyContent="center" alignItems="flex-start">
-          <Spacing smart={{margin: "5vh 0 10vh 0"}}>
+        <Grid container direction="column" justifyContent="center" alignItems="center">
+          <Spacing style={{maxWidth: 944}} desktop={{margin: "160px 0px 0px 0px"}} smart={{margin: "40px 0px"}}>
             <Grid item xs={12} lg={6}>
-              <Typography className={classes.fontSection} variant="h2" component="h2" color="secondary"> Princípios </Typography>
-            </Grid>
-          </Spacing>
-          <Spacing smart={{margin: "0px 0px 40px"}}>
-            <Grid item xs={12}>
-              <Rule title="Todos são bem-vindos">
-                Os participantes do Hacktoberfest representaram 151 países e reuniu milhares de habilidades únicas. Damos as boas-vindas a todos que já fazem parte da comunidade de software de código aberto e todos que estão interessados em mergulhar nesse universo.
-              </Rule>
-            </Grid>
-          </Spacing>
-          <Spacing smart={{margin: "0px 0px 40px"}}>
-            <Grid item>
-              <Rule title="A quantidade é divertida, a qualidade é a chave.">
-                Participar do Hacktoberfest leva ao crescimento pessoal, oportunidades profissionais e construção de comunidade. No entanto, tudo começa com contribuições significativas para o software de código aberto.
-              </Rule>
-            </Grid>
-          </Spacing>
-          <Grid item>
-            <Rule title="Ação de curto prazo, impacto de longo prazo.">
-              Na comunidade de software de código aberto, estamos nos apoiando nos ombros daqueles que vieram antes de nós. Sua participação tem um efeito duradouro nas pessoas e na tecnologia, muito depois de outubro. Esta é uma viagem, não uma corrida.
-            </Rule>
-          </Grid>
-          <Spacing smart={{margin: "64px 0px 40px"}}>
-            <Grid item xs={12}>
-              <Typography variant="h2" className={classes.fontSection} component="h2" color="secondary"> Regras </Typography>
-            </Grid>
-          </Spacing>
-          <Spacing smart={{margin: "0px 0px 40px"}}>
-            <Grid item>
-              <Rule> Para obter uma camiseta, você deve ter dois pull requests (PRs) enviados entre 1 e 31 de Outubro e pelo menos um deles aprovado.</Rule>
-            </Grid>
-          </Spacing>
-          <Spacing smart={{margin: "0px 0px 40px"}}>
-            <Grid item>
-              <Rule> Os pull requests podem ser feitos em qualquer repositório dos projetos open source da Globo, não apenas para aqueles destacados. </Rule>
-            </Grid>
-          </Spacing>
-          <Spacing smart={{margin: "0px 0px 40px"}}>
-            <Grid item>
-              <Rule> O PR deve conter confirmações que você mesmo fez. </Rule>
-            </Grid>
-          </Spacing>
-          <Spacing smart={{margin: "0px 0px 40px"}}>
-            <Grid item>
-              <Rule> Se um mantenedor reportar seu PR como spam, o mesmo não será contabilizado para sua participação no Hacktoberfest. </Rule>
-            </Grid>
-          </Spacing>
-          <Spacing smart={{margin: "0px 0px 100px"}}>
-            <Grid item>
-              <Rule> Se um mantenedor reportar um comportamento que não esteja de acordo com o código de conduta do projeto, você não poderá participar. </Rule>
+              <Grid container>
+                <Spacing smart={{margin: "5vh 0 10vh 0"}}>
+                  <Grid item xs={12} lg={6}>
+                    <Typography className={classes.fontSection} variant="h2" component="h2" color="secondary"> Princípios </Typography>
+                  </Grid>
+                </Spacing>
+                <Spacing smart={{margin: "0px 0px 40px"}}>
+                  <Grid item xs={12}>
+                    <Rule title="Todos são bem-vindos">
+                      Os participantes do Hacktoberfest representaram 151 países e reuniu milhares de habilidades únicas. Damos as boas-vindas a todos que já fazem parte da comunidade de software de código aberto e todos que estão interessados em mergulhar nesse universo.
+                    </Rule>
+                  </Grid>
+                </Spacing>
+                <Spacing smart={{margin: "0px 0px 40px"}}>
+                  <Grid item>
+                    <Rule title="A quantidade é divertida, a qualidade é a chave.">
+                      Participar do Hacktoberfest leva ao crescimento pessoal, oportunidades profissionais e construção de comunidade. No entanto, tudo começa com contribuições significativas para o software de código aberto.
+                    </Rule>
+                  </Grid>
+                </Spacing>
+                <Grid item>
+                  <Rule title="Ação de curto prazo, impacto de longo prazo.">
+                    Na comunidade de software de código aberto, estamos nos apoiando nos ombros daqueles que vieram antes de nós. Sua participação tem um efeito duradouro nas pessoas e na tecnologia, muito depois de outubro. Esta é uma viagem, não uma corrida.
+                  </Rule>
+                </Grid>
+                <Spacing smart={{margin: "64px 0px 40px"}}>
+                  <Grid item xs={12}>
+                    <Typography variant="h2" className={classes.fontSection} component="h2" color="secondary"> Regras </Typography>
+                  </Grid>
+                </Spacing>
+                <Spacing smart={{margin: "0px 0px 40px"}}>
+                  <Grid item>
+                    <Rule> Para obter uma camiseta, você deve ter dois pull requests (PRs) enviados entre 1 e 31 de Outubro e pelo menos um deles aprovado.</Rule>
+                  </Grid>
+                </Spacing>
+                <Spacing smart={{margin: "0px 0px 40px"}}>
+                  <Grid item>
+                    <Rule> Os pull requests podem ser feitos em qualquer repositório dos projetos open source da Globo, não apenas para aqueles destacados. </Rule>
+                  </Grid>
+                </Spacing>
+                <Spacing smart={{margin: "0px 0px 40px"}}>
+                  <Grid item>
+                    <Rule> O PR deve conter confirmações que você mesmo fez. </Rule>
+                  </Grid>
+                </Spacing>
+                <Spacing smart={{margin: "0px 0px 40px"}}>
+                  <Grid item>
+                    <Rule> Se um mantenedor reportar seu PR como spam, o mesmo não será contabilizado para sua participação no Hacktoberfest. </Rule>
+                  </Grid>
+                </Spacing>
+                <Spacing smart={{margin: "0px 0px 100px"}}>
+                  <Grid item>
+                    <Rule> Se um mantenedor reportar um comportamento que não esteja de acordo com o código de conduta do projeto, você não poderá participar. </Rule>
+                  </Grid>
+                </Spacing>
+              </Grid>
             </Grid>
           </Spacing>
         </Grid>
