ee4d18e92dc1188929046f103fc5e03d495680b7
Pedro Paulo <pedro.pereira@g.globo>
2023-09-12 17:16:00 -0300

    fix button hover in minha area

--- src/components/loading-button/LoadingButton.tsx
+++ src/components/loading-button/LoadingButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button, CircularProgress } from '@material-ui/core'
+import { makeStyles, Theme } from "@material-ui/core/styles"
 
 
 interface LoadingButtonProps {
@@ -8,10 +9,20 @@ interface LoadingButtonProps {
     isLoading: boolean
 }
 
+const useStyles = makeStyles((theme: Theme) => ({
+   button: {
+    width: "100%",
+    backgroundColor: theme.palette.secondary.main,
+    fontFamily: "Globotipo Variable",
+    borderRadius: "8px",
+    textTransform: "none",
+  },
+}))
 const LoadingButton = (props: LoadingButtonProps) => {
+    const classes = useStyles()
 
     return (
-        <Button style={{textTransform: "lowercase", fontWeight: 600}} fullWidth disabled={props.isLoading} type="submit" variant="contained" color="secondary" size="large">
+        <Button className={classes.button} fullWidth disabled={props.isLoading} type="submit" variant="contained" size="large">
             {props.isLoading ? "Processando..." : props.children}
             {props.isLoading && <CircularProgress size="1.1rem" color="inherit"/> }
         </Button>
