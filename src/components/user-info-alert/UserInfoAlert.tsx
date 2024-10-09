89af0c30b098b816f9ee2e161da6346b58fe25c9
Lucas Barros <lucasbarros@Lucass-MacBook-Pro.local>
2024-10-09 15:33:58 -0300

    Adjust cookie validation and message layout

--- src/components/user-info-alert/UserInfoAlert.tsx
+++ src/components/user-info-alert/UserInfoAlert.tsx
@@ -1,32 +1,36 @@
 import React, { useEffect, useState }  from "react"
-import { 
-  Snackbar, 
-  SnackbarContent, 
-  Button, 
-  makeStyles, 
-  Theme 
+import {
+  Snackbar,
+  SnackbarContent,
+  Button,
+  makeStyles,
+  Theme
 } from "@material-ui/core"
-import { setCookie, getCookie } from "@helpers/cookiesHelper"
+import { setCookie, getCookie, getCookieExpiration } from "@helpers/cookiesHelper"
 
 const COOKIE_NAME = "ACCEPT_GLOBO_HACKTOBERFEST_LGPD"
+const COOKIE_EXPIRATION = 330
 
 const useStyles = makeStyles((theme: Theme) => ({
   snackbar: {
-    backgroundColor: "#fff", 
+    backgroundColor: "#DDF2FF",
     color: '#000',
-    borderRadius: 8,
+    borderRadius: "8px",
+    fontSize: 16,
+    fontWeight: 400,
+    lineHeight: "24px",
+    textAlign: "left",
   },
   button: {
-    backgroundColor: theme.palette.primary.main,
-    color: "#fff",
-    borderRadius: 8,
-    "&:hover": {
-      backgroundColor: theme.palette.primary.dark,
-    }
+    backgroundColor: "#07B1EF4D",
+    fontFamily: "inherit",
+    borderRadius: "2px",
+    border: "3px solid #02BBFF",
+    color: "#2E3192",
   }
 }))
 
-const snackbarMessage: string = 
+const snackbarMessage: string =
   "Nós usamos as informações do seu cadastro para fins de \
   comunicação sobre as edições passadas, corrente e futuras \
   do evento e também sobre a premiação e outros assuntos \
@@ -37,11 +41,15 @@ const UserInfoAlert = () => {
   const classes = useStyles()
 
   useEffect(() => {
-    if (!getCookie(COOKIE_NAME)) setHideSnackbar(false)
+    const cookie = getCookie(COOKIE_NAME);
+    const expirationDate = getCookieExpiration();
+    if (!cookie || (expirationDate && expirationDate < new Date())) {
+      setHideSnackbar(false);
+    }
   }, []);
 
   const handleClick = () => {
-    setCookie(COOKIE_NAME, "true")
+    setCookie(COOKIE_NAME, "true", COOKIE_EXPIRATION)
     setHideSnackbar(true)
   }
 
